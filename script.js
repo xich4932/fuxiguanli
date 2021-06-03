bae5017885b6117f9ed6b66a5ac5d96652edbf8c
xich4932 <xich4932@colorado.du>
2021-06-04 00:54:23 +0800

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function(){
     var table = document.getElementById("table_body")
-    table.innerHTML;
+    //table.innerHTML;
     str = ''
     count = 0;
     window_time = ["6:00-8:00", "8:00-10:00", "10:00-12:00", "12:00-14:00", "14:00-16:00", "16:00-18:00", "18:00-20:00", "20:00-22:00", "22:00-24:00"]
@@ -12,7 +12,8 @@ window.onload = function(){
         }
         str += '</tr>'
     }
-    table.innerHTML += str;
+    //document.querySelector("#final_date0").valueAsDate = new Date();
+    table.innerHTML += str
 };
 
 function checking(){
@@ -27,3 +28,18 @@ function checking(){
         }
     }
 }
+
+var total = 1;
+
+function add_course(){
+    add = document.getElementById("course");
+    total += 1
+    str = '<tr><th scope="row">'+ total+'</th><td><input class="form-control" type="text" placeholder="input course name" aria-label="default input example"></td><td><input type="date" id="final_date0"></td><td><input type="number" id="num_course"></td></tr>'
+    $('#add_course_table tr:last').after(str);
+
+}
+
+function generator() {
+    
+}
+
