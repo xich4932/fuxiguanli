c451ec6f7ebb1668d5580584710aae4ee4228a06
xich4932 <xich4932@colorado.du>
2021-06-07 19:59:42 +0800

    2nd

--- script.js
+++ script.js
@@ -12,16 +12,10 @@ window.onload = function(){
         }
         str += '</tr>'
     }
-    //document.querySelector("#final_date0").valueAsDate = new Date();
     table.innerHTML += str
 };
 
 function checking(){
-    console.log("calling function")
-    var a = 2;
-    console.log('count' + a)
-    var temp1 = document.getElementById('check12')
-    var temp2 = document.getElementById('check23')
     for( i = 0; i <63; i++ ){
         if(document.getElementById('check' + String(i)).checked == true){
             console.log(i)
@@ -32,14 +26,52 @@ function checking(){
 var total = 1;
 
 function add_course(){
-    add = document.getElementById("course");
+    /*
+    rows = document.getElementById("nums").value
+    console.log(rows)
+    
     total += 1
-    str = '<tr><th scope="row">'+ total+'</th><td><input class="form-control" type="text" placeholder="input course name" aria-label="default input example"></td><td><input type="date" id="final_date0"></td><td><input type="number" id="num_course"></td></tr>'
+    str = ""
+    for(var d = 0; d < rows; d++){
+        str += '<tr><th scope="row">'+d+'</th><td><input class="form-control" type="text" placeholder="input course name" aria-label="default input example"></td><td><input type="date" id="final_date'+d+'"></td><td><input type="number" id="num_course"></td></tr>'
+    }
+    */
+    add = document.getElementById("course");
+    str = '<tr><th scope="row">'+ (total+1)+'</th><td><input class="form-control" type="text" placeholder="input course name" aria-label="default input example" id="course_'+total+'"></td><td><input type="date" id="final_'+total+'"></td><td><input type="number" id="num_'+total+'"></td></tr>'
+    total += 1
     $('#add_course_table tr:last').after(str);
+    //add.innerHTML = str;
+   /*
+    var tab = document.getElementById("course_table");
+    var n = document.getElementById("final_date0").rowIndex + 1
+    var tr = tab.insertRow(n)
+    var td = tr.insertCell(0)
+    td.innerHTML='new '+Math.random();
+    */
+}
+   
+function printing(){
+    console.log(document.getElementById('final_date1').value)
+    console.log(document.getElementById('num_course1').value)
+    console.log(document.getElementById('course_1').value)
 
 }
 
-function generator() {
+function exclude_from(var arr[]){
+    
+}
+
+function create() {
+    var final_arr=[];
+    for(var i = 0; i < total; i++){
+        final_arr.push(document.getElementById("final_"+String(i)).value)
+        //console.log(document.getElementById("final_"+String(i)).value)
+    }
+    for(var i = 0; i < total; i++){
+        //final_arr.push(document.getElementById("final_"+String(i)))
+        console.log(final_arr[i])
+    }
+    
     
 }
 
